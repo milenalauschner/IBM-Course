ef99ee5d1bb99827e72e9d688ba06635e3fe8af3
Milena Lauschner <66431668+milenalauschner@users.noreply.github.com>
2025-06-17 18:24:05 -0300

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -8,7 +8,7 @@ Mary went Everywhere that Mary went The lamb was sure to go"**
 string = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb.Its fleece was white as snow And everywhere that Mary went Mary went, Mary went Everywhere that Mary went The lamb was sure to go"
 key = "little"
 
-def freq (string, key):
+def word_count (string, key):
     words = string.lower().replace(",", "").replace(".", "")split()
     Dict = {}
 
@@ -16,12 +16,12 @@ def freq (string, key):
     for word in words:
         Dict[word] = freq_dict.get(word, 0) + 1
       # Dict[word] stores the updated count back into the dictionary using square brackets.
-      #.get function os used to check if an especific attribute is exist in a dict
+      #.get function os used to check if an specific attribute exists in a dictionary.
     
   
     print(f'Total count of the word {key} in the string is: {Dict[key]}')
 
     #return Dict #retrieves the total count for all words in the string
-    return Dict[key] #retrieces the total count for a specific work in the string
+    return Dict[key] #retrieves the total count for a specific word in the string
 
-freq(string, key)
+word_count (string, key)
